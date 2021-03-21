7193428b08a89b0bbadb73406991f37dc1cdd169
Thomas Vilhena <thomas.vilhena@gmail.com>
2021-03-21 15:38:54 -0300

    Using "dotenv" for managing environment variables

--- migrations/3_deploy_feeds.js
+++ migrations/3_deploy_feeds.js
@@ -6,7 +6,18 @@ module.exports = async function(deployer) {
   //   feed, 
   //   "BTC/USD", 
   //   "0x6135b13325bfC4B00278B4abC5e20bbce2D6580e",
-  //   "0x12782eec48681A668Fa3450F781C858f7886A591",
+  //   "0x3f8171D562c76d49dCdFD99a2F36C040CCb7d7A3",
+  //   3 * 60 * 60,
+  //   [],
+  //   []
+  // );
+
+  // await deployer.deploy(
+  //   feed, 
+  //   "ETH/USD", 
+  //   "0x9326BFA02ADD2366b30bacB125260Af641031331",
+  //   "0x3f8171D562c76d49dCdFD99a2F36C040CCb7d7A3",
+  //   3 * 60 * 60,
   //   [],
   //   []
   // );
--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,21 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+
+require('dotenv').config();
 
 module.exports = {
 
   networks: {
-
-  },
-
-  mocha: {
-    // timeout: 100000
+    kovan: {
+      provider: function() {
+        return new HDWalletProvider(
+          process.env.MNENOMIC,
+          "wss://kovan.infura.io/ws/v3/" + process.env.INFURA_API_KEY
+        )
+      },
+      network_id: 42,
+      networkCheckTimeout: 1000000,
+      timeoutBlocks: 200
+    }
   },
 
   compilers: {
@@ -15,9 +24,17 @@ module.exports = {
       settings: {
         optimizer: {
           enabled: true,
-          runs: 200
+          runs: 500
         }
       }
     }
+  },
+  
+  plugins: [
+    'truffle-plugin-verify'
+  ],
+
+  api_keys: {
+    etherscan: process.env.ETHERSCAN_KEY
   }
 };
